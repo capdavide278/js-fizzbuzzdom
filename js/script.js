728392ea0e2c7220eb3b9d2b873219e709b2024f
Davide <franc.davi00@gmail.com>
2022-05-05 15:55:43 +0200

    inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100.
+MILESTONE 1
+Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+MILESTONE 2
+Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+MILESTONE 3
+Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.*/ 
+
+//prima milestone
+for (let i = 1; i <= 100; i++){
+    console.log("ecco il tuo numero: " +i);
+
+     if(i % 3 == 0){
+        console.log("Fizz");
+
+    } else if (i % 5 == 0){
+        console.log("Buzz");
+
+    } if ((i % 3 == 0) && (i % 5 == 0)){
+        console.log("FizzBuzz");
+    }
+
+}
